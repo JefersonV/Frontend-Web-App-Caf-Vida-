4074cd092bb23c04bc1e5f22b54baf35ddb259e0
Donald <thomaschavajay20@gmail.com>
2022-10-04 16:30:04 -0600

    "Login con SessionStorage"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import SideBarMenu from "./components/SideBarMenu"
-import { BrowserRouter,Routes,Route} from "react-router-dom";
+import SideBarMenu from "./components/SideBarMenu";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sales from "./pages/Sales";
 import Home from "./pages/Home";
 import Orders from "./pages/Orders";
@@ -13,30 +13,49 @@ import Providers from "./pages/Providers";
 import Shopping from "./pages/Shopping";
 import CustomerReturns from "./pages/CustomerReturns";
 import ProvidersReturns from "./pages/ProvidersReturns";
+import Login from "./pages/Login";
+
+//Librerias para el login
+import useToken from "./components/useToken";
+
+//NOTA: para borrar el token de sessionStorage utilizamos
+//sessionStorage.clear()
 
 function App() {
+  const { token, setToken } = useToken();
+
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
+
   return (
-  <>
-    <BrowserRouter>
-      <SideBarMenu/>
+    <>
+      <BrowserRouter>
+        <SideBarMenu />
         <Routes>
-          <Route exact path="/" element={ <Home/> }/>
-          <Route path="/sales" element={<Sales/>}/>
-          <Route path="/inventory" element={<Inventory/>} />
-          <Route path="/inventory/products" element={<Products/>} />
-          <Route path="/inventory/providers" element={<Providers/>} />
-          <Route path="/inventory/shopping" element={<Shopping/>} />
-          <Route path="/inventory/sales_returns" element={<CustomerReturns/>} />
-          <Route path="/inventory/shopping_returns" element={<ProvidersReturns/>} />
+          <Route exact path="/" element={<Home />} />
+          <Route path="/sales" element={<Sales />} />
+          <Route path="/inventory" element={<Inventory />} />
+          <Route path="/inventory/products" element={<Products />} />
+          <Route path="/inventory/providers" element={<Providers />} />
+          <Route path="/inventory/shopping" element={<Shopping />} />
+          <Route
+            path="/inventory/sales_returns"
+            element={<CustomerReturns />}
+          />
+          <Route
+            path="/inventory/shopping_returns"
+            element={<ProvidersReturns />}
+          />
           <Route path="/orders" element={<Orders />} />
-          <Route path="/production_cost" element={<Production/>} />
-          <Route path="/customers" element={<Customers/>} />
-          <Route path="/users" element={<AdminUsers/>} />
-          <Route path="/reports" element={<Reports/>} />
+          <Route path="/production_cost" element={<Production />} />
+          <Route path="/customers" element={<Customers />} />
+          <Route path="/users" element={<AdminUsers />} />
+          <Route path="/reports" element={<Reports />} />
         </Routes>
-    </BrowserRouter>
-  </>
-  )
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
