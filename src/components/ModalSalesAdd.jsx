d5d15f593b25a7d6a52adb974299f2dff9e7a926
Jeferson <jvelasquezc15@miumg.edu.gt>
2022-10-08 13:30:08 -0600

    Estilos arreglados de los modales, para que no interfiera uno con el otro

--- src/components/ModalSalesAdd.jsx
+++ src/components/ModalSalesAdd.jsx
@@ -4,66 +4,67 @@ import {BiEdit} from 'react-icons/bi'
 import '../assets/styles/Sales.css'
 import { Link } from 'react-router-dom';
 
-
-
 const ModalSalesAdd = ({children, estado2, cambiarEstado2}) => {
   
-  
-    return (
-    <>
-    {estado2 &&  
-      <Overlay1> 
+  const onSubmitData = (e) => {
+    console.log(e)
+  }
+  return (
 
-        <ContenedorModal>       
-            <h1><BiEdit size='2rem' color='darkgreen'/>Registro de clientes </h1>
-            <Form >
+  <>
+  {estado2 &&  
+    <Overlay1> 
 
-            <label htmlFor="" className='lal1'> Código </label>
-              <div className='boddy'>
-              <input className='txt1' type="text" placeholder=' Código autogenerado' required /> 
-              </div>
+      <ContenedorModal>       
+          <h1><BiEdit size='2rem' color='darkgreen'/>Registro de clientes </h1>
+          <Form onSubmit={() => onSubmitData}>
 
-            <label htmlFor="" className='lal2'> Nombre </label>
-              <div className='boddy'>
-              <input className='txt1' type="text" placeholder=' Ingrese nombre' required /> 
-              </div>
+          <label htmlFor="" className='lal1'> Código </label>
+            <div className='boddy'>
+            <input className='txt1' type="text" placeholder=' Código autogenerado' required disabled /> 
+            </div>
 
-              <label htmlFor="" className='lal3'> Teléfono </label>
-              <div className='boddy3'>
-              <input className='txt2' type="phone" placeholder=' Ingrese numero' required /> 
-              <input className='txt3' type="phone" placeholder=' Ingrese numero' required /> 
-              </div>
+          <label htmlFor="" className='lal2'> Nombre </label>
+            <div className='boddy'>
+            <input className='txt1' type="text" placeholder=' Ingrese nombre' required /> 
+            </div>
+
+            <label htmlFor="" className='lal3'> Teléfono </label>
+            <div className='boddy3'>
+            <input className='txt2' type="phone" placeholder=' Ingrese numero' required /> 
+            <input className='txt3' type="phone" placeholder=' Ingrese numero' required /> 
+            </div>
 
 
-              <label htmlFor="" className='la4'> NIT</label>
-              <div className='boddy4'>
-              <input className='txt4' type="text" placeholder=' Ingrese NIT' required /> 
-              </div>
+            <label htmlFor="" className='la4'> NIT</label>
+            <div className='boddy4'>
+            <input className='txt4' type="text" placeholder=' Ingrese NIT' required /> 
+            </div>
 
-             <label htmlFor="" className='lal5'> Dirección </label>
-              <div className='boddy5'>
-              <input className='txt5' type="text" placeholder=' Ingrese Dirección' required /> 
-              </div>
+          <label htmlFor="" className='lal5'> Dirección </label>
+            <div className='boddy5'>
+            <input className='txt5' type="text" placeholder=' Ingrese Dirección' required /> 
+            </div>
 
-              <LinkButt>
-              <Link to="" className='btn8' onClick={()=> cambiarEstado2(false)}>  Cancelar</Link>
-              <Link  to="" className='btn9'>  Guardar </Link>
-              </LinkButt>
+            <LinkButt>
+            <Link to="" className='btn8' onClick={()=> cambiarEstado2(false)}>  Cancelar</Link>
+            <Link  to="" className='btn9'>  Guardar </Link>
+            </LinkButt>
 
-        {children}
-            </Form>   
-            
+      {children}
+          </Form>   
           
-
-        </ContenedorModal>       
-
-      
-      </Overlay1> }
-      
         
-    </>
+
+      </ContenedorModal>       
+
     
-  )
+    </Overlay1> }
+    
+      
+  </>
+  
+)
   
 }
 export default ModalSalesAdd
@@ -83,16 +84,16 @@ const Overlay1 = styled.div `
     
 `;
 const ContenedorModal = styled.div`
-       width: 550px;
-       height: 500px;
+      width: 550px;
+      height: 500px;
       padding: 20px;
-       background: #fff;
-       position: relative;
-       margin-right: 80px;
-       margin-left: 100px;
-       bottom: 30px;
-       
-       box-shadow: rgba(100,100,111, 0.2) 0px 7px 29px 0px;
+      background: #fff;
+      position: relative;
+      margin-right: 80px;
+      margin-left: 100px;
+      bottom: 30px;
+      
+      box-shadow: rgba(100,100,111, 0.2) 0px 7px 29px 0px;
       h1{
         
         margin-top: 0;
@@ -138,9 +139,8 @@ box-shadow: 3px 3px 7px rgb(75, 33, 122);
   padding: 5px;
   margin-left: 100px;
   background-color: rgba(230, 24, 24, 0.897);
-   border: solid 1px;
+  border: solid 1px;
   box-shadow: 3px 3px 7px rgb(75, 33, 122);
-
 }
 `;
 
